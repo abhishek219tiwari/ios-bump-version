23182c39fd8d5705215eaac72d654a24169f940a
noman.ansari <noman.ansari@nitorinfotech.com>
2022-10-13 13:21:23 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ if ! type fastlane > /dev/null 2>&1; then
     sudo gem install fastlane -NV
   fi
 fi
-
+fastlane add_plugin versioning
 script_path=$(cd $(dirname ${0}); pwd)
 cp -r ${script_path}/fastlane ./
-# bundle exec fastlane add_plugin versioning
 fastlane bump
