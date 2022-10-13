70549b3e7d06031cb30dc72870801078a5c6f51a
noman.ansari <noman.ansari@nitorinfotech.com>
2022-10-13 10:26:26 +0530

    ios-bump-version

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if ! type fastlane > /dev/null 2>&1; then
+  if type brew > /dev/null 2>&1; then
+    brew install fastlane
+  else
+    sudo gem install fastlane -NV
+  fi
+fi
+
+script_path=$(cd $(dirname ${0}); pwd)
+cp -r ${script_path}/fastlane ./
+fastlane add_plugin versioning
+fastlane bump
---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    if (
+      !core.getInput("marketingVersion") &&
+      !core.getInput("project-path") &&
+      !core.getInput("Scheme")
+    ) {
+      throw new Error(
+        "make sure you provided marketingVersion, project-path and scheme"
+      );
+    }
+    process.env.MARKET_VERSION = core.getInput("marketingVersion");
+    process.env.PROJECT_PATH = core.getInput("project-path");
+    process.env.SCHEME = core.getInput("scheme");
+    await exec.exec(`bash ${__dirname}/../build.sh`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
